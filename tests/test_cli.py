2a4ff201a52c516c96f2d72c0e3b07abb479f49f
Zoran Simic <zsimic@netflix.com>
2025-10-29 13:55:08 -0700

    Use std lib only

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,35 @@
-def test_cli(cli):
+import json
+
+
+def test_folder(cli):
     cli.run(cli.project_folder)
     assert cli.succeeded
-    assert "uv pip show uv-metadata" in cli.logged.stderr
-    assert '"uv-metadata": "uv_metadata:main"' in cli.logged.stdout
+    payload = json.loads(cli.logged.stdout.contents())
+    assert payload["entry_points"]["console_scripts"]["uv-metadata"] == "uv_metadata:main"
+    assert payload["name"] == "uv-metadata"
+    assert payload["top_level"] == ["uv_metadata"]
+
+
+def test_package(cli):
+    cli.run("pip")
+    assert cli.succeeded
+    payload = json.loads(cli.logged.stdout.contents())
+    assert "pip" in payload["entry_points"]["console_scripts"]
+    assert payload["name"] == "pip"
+    assert payload["version"]
+
+
+def test_single_key(cli):
+    cli.run(cli.project_folder, "-kname")
+    assert cli.succeeded
+    assert cli.logged.stdout.contents().strip() == "uv-metadata"
+
+    cli.run(cli.project_folder, "-kname2")
+    assert cli.failed
+    assert "'name2' not found in metadata" in cli.logged
+
+
+def test_bogus(cli):
+    cli.run("a--")
+    assert cli.failed
+    assert "failed with exit code" in cli.logged
--- uv_metadata.py
+++ uv_metadata.py
@@ -1,77 +1,99 @@
-import logging
+import argparse
+import configparser
+import json
 import os
+import re
+import shutil
+import subprocess
 import sys
+import tempfile
 from email.parser import Parser
-
-import click
-import runez
-from runez.pyenv import PypiStd
+from pathlib import Path
 
 CURRENT_PY = f"{sys.version_info[0]}.{sys.version_info[1]}"
 
 
+def abort(msg: str):
+    sys.exit(msg)
+
+
+def run_command(exe, *args, fatal=True):
+    exe = shutil.which(exe)
+    full_cmd = [exe, *args]
+    result = subprocess.run(full_cmd, capture_output=True, text=True, check=False)
+    if fatal and result.returncode:
+        description = "%s %s" % (exe, " ".join(str(x) for x in args))
+        abort(f"'{description}' failed with exit code {result.returncode}:\n{result.stderr}")
+
+    return result
+
+
+def snakified(name: str) -> str:
+    return re.sub(r"[^\w]", "_", name).lower()
+
+
+def get_metadata_dict(path):
+    result = {}
+    if path.exists():
+        parser = Parser()
+        with open(path, "r") as fh:
+            raw = parser.parse(fh, headersonly=False)
+            for key, value in raw.items():
+                if key == "Dynamic":
+                    continue
+
+                if key not in result:
+                    result[key] = value
+                    continue
+
+                prev = result.get(key)
+                if isinstance(prev, list):
+                    prev.append(value)
+
+                else:
+                    result[key] = [prev, value]
+
+    result = {snakified(k): v for k, v in result.items()}
+    return result
+
+
 def get_metadata(pip_spec: str, python: str) -> dict:
-    with runez.TempFolder():
-        runez.run("uv", "venv", f"-p{python}")
-        runez.run("uv", "pip", "install", "--no-deps", pip_spec)
-        r = runez.run("uv", "pip", "freeze")
-        lines = r.output.splitlines()
-        runez.abort_if(not lines or len(lines) != 1, f"'pip freeze' for '{pip_spec}' failed: {r.full_output}")
-        if "@" in lines[0]:
-            package_name = lines[0].partition("@")[0].strip()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        os.chdir(tmpdir)
+        run_command("uv", "venv", f"-p{python}")
+        run_command("uv", "pip", "install", "--no-deps", pip_spec)
+        r = run_command("uv", "pip", "freeze")
+        package_name = r.stdout.splitlines()[0]
+        if "@" in package_name:
+            package_name = package_name.partition("@")[0].strip()
 
         else:
-            package_name = lines[0].partition("=")[0].strip()
+            package_name = package_name.partition("=")[0].strip()
 
-        r = runez.run("uv", "pip", "show", package_name)
-        show_metadata = Parser().parsestr(r.output)
+        r = run_command("uv", "pip", "show", package_name)
+        show_metadata = Parser().parsestr(r.stdout)
         name = show_metadata["Name"]
-        location = runez.to_path(show_metadata["Location"])
         version = show_metadata["Version"]
-        wheel_name = PypiStd.std_wheel_basename(name)
-        dist_info_name = f"{wheel_name}-{version}.dist-info"
-        full_path = location / dist_info_name
+        wheel_name = snakified(name)
+        full_path = Path(show_metadata["Location"]) / f"{wheel_name}-{version}.dist-info"
         metadata = get_metadata_dict(full_path / "METADATA")
-        entry_points = runez.file.ini_to_dict(full_path / "entry_points.txt")
-        if entry_points:
-            metadata["entry_points"] = entry_points
+        entry_points = full_path / "entry_points.txt"
+        if entry_points.exists():
+            config = configparser.ConfigParser()
+            config.read(entry_points)
+            eps = {section: dict(config.items(section)) for section in config.sections()}
+            if eps:
+                metadata["entry_points"] = eps
 
-        top_levels = list(runez.file.readlines(full_path / "top_levels.txt"))
-        if top_levels:
-            metadata["top_levels"] = top_levels
+        top_level = full_path / "top_level.txt"
+        if top_level.exists():
+            with open(top_level, "r") as fh:
+                metadata["top_level"] = fh.read().splitlines()
 
         return metadata
 
 
-def get_metadata_dict(path):
-    parser = Parser()
-    result = {}
-    with open(path, "r") as fh:
-        raw = parser.parse(fh, headersonly=False)
-        for key, value in raw.items():
-            if key == "Dynamic":
-                continue
-
-            if key not in result:
-                result[key] = value
-                continue
-
-            prev = result.get(key)
-            if isinstance(prev, list):
-                prev.append(value)
-
-            else:
-                result[key] = [prev, value]
-
-    result = {runez.snakified(k, normalize=str.lower): v for k, v in result.items()}
-    return result
-
-
-@runez.click.command()
-@runez.click.debug()
-@click.option("--python", "-p", default=CURRENT_PY, help=f"Python interpreter to use (default: {CURRENT_PY})")
-@click.argument("package", default=".")
-def main(debug, python, package):
+def main(args=None):
     """
     Output the metadata of a package, in machine-readable format
 
@@ -84,10 +106,30 @@ def main(debug, python, package):
     # Ensure we don't let our own venv interfere with uv usage
     os.environ.pop("VIRTUAL_ENV", None)
     os.environ["UV_VENV_SEED"] = "0"
-    if package.startswith((".", "/", "~")):
-        package = runez.resolved_path(package)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--python", "-p", default=CURRENT_PY, help=f"Python interpreter to use (default: {CURRENT_PY})")
+    parser.add_argument("--key", "-k", help="Show only this key from the metadata")
+    parser.add_argument("package", default=".", nargs="?", help="Show metadata for this package")
+    args = parser.parse_args(args=args)
 
-    runez.log.setup(debug=debug, console_level=logging.INFO)
-    with runez.log.timeit("Get metadata", logger=logging.info):
-        metadata = get_metadata(package, python)
-        print(runez.represented_json(metadata))
+    key = args.key
+    package = args.package
+    python = args.python
+    if package.startswith((".", "/", "~")):
+        package = (Path(package).expanduser()).absolute()
+
+    metadata = get_metadata(package, python)
+    if key:
+        if key not in metadata:
+            abort(f"'{key}' not found in metadata")
+
+        text = metadata[key]
+
+    else:
+        text = json.dumps(metadata, indent=4, sort_keys=True)
+
+    print(text)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(main())
