6d3af68a744e9f13db1642082ba1ecbeb492c12f
Zoran Simic <zsimic@netflix.com>
2025-10-29 15:25:43 -0700

    Added docs

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # uv-metadata
+
 Python proof-of-concept implementation of a `uv metadata` subcommand
+
+The script uses std lib only, so you should be able to easily try it out.
+You need `uv` available on `PATH`.
+
+The intent is to provide metadata information on a single python package,
+given a PEP-508 compliant spec.
+
+Examples:
+
+- Info on a published package
+
+```shell
+~: uv-metadata requests
+{
+    "author": "Kenneth Reitz",
+    ...
+    "name": "requests",
+    "requires_dist": [
+        "charset_normalizer<4,>=2",
+        ...
+    "requires_python": ">=3.9",
+    "version": "2.32.5"
+}
+
+~: uv-metadata 'requests<2'
+{
+    ...
+    "version": "1.2.3"
+}
+```
+
+- Info on a git folder:
+
+```
+~: uv-metadata git+https://github.com/zsimic/uv-metadata@main
+{
+    "entry_points": {
+        "console_scripts": {
+            "uv-metadata": "uv_metadata:main"
+        }
+    },
+    "license_file": "LICENSE",
+    "metadata_version": "2.4",
+    "name": "uv-metadata",
+    "top_level": [
+        "uv_metadata"
+    ],
+    "version": "0.1.0"
+}
+```
+
+
+- Info on a local folder
+
+```
+~: uv-metadata .
+{
+    ...
+    "name": "...",
+    "version": "..."
+}
+```
--- uv_metadata.py
+++ uv_metadata.py
@@ -10,20 +10,19 @@ import tempfile
 from email.parser import Parser
 from pathlib import Path
 
-CURRENT_PY = f"{sys.version_info[0]}.{sys.version_info[1]}"
 
-
-def abort(msg: str):
-    sys.exit(msg)
+def abort_if(condition, msg: str):
+    if condition:
+        sys.exit(msg)
 
 
 def run_command(exe, *args, fatal=True):
     exe = shutil.which(exe)
     full_cmd = [exe, *args]
     result = subprocess.run(full_cmd, capture_output=True, text=True, check=False)
-    if fatal and result.returncode:
+    if result.returncode:
         description = "%s %s" % (exe, " ".join(str(x) for x in args))
-        abort(f"'{description}' failed with exit code {result.returncode}:\n{result.stderr}")
+        abort_if(fatal, f"'{description}' failed with exit code {result.returncode}:\n{result.stderr}")
 
     return result
 
@@ -53,17 +52,23 @@ def get_metadata_dict(path):
                 else:
                     result[key] = [prev, value]
 
-    result = {snakified(k): v for k, v in result.items()}
+        result = {snakified(k): v for k, v in result.items()}
+
     return result
 
 
 def get_metadata(pip_spec: str, python: str) -> dict:
+    os.environ["UV_VENV_SEED"] = "0"
     with tempfile.TemporaryDirectory() as tmpdir:
-        os.chdir(tmpdir)
-        run_command("uv", "venv", f"-p{python}")
+        tmpdir = Path(tmpdir)
+        venv_folder = tmpdir / ".venv"
+        os.environ["VIRTUAL_ENV"] = str(venv_folder)
+        run_command("uv", "venv", f"-p{python}", venv_folder)
         run_command("uv", "pip", "install", "--no-deps", pip_spec)
         r = run_command("uv", "pip", "freeze")
-        package_name = r.stdout.splitlines()[0]
+        frozen = r.stdout.splitlines()
+        abort_if(len(frozen) != 1, f"Unexpected pip freeze output:\n{r.stdout}")
+        package_name = frozen[0]
         if "@" in package_name:
             package_name = package_name.partition("@")[0].strip()
 
@@ -72,9 +77,8 @@ def get_metadata(pip_spec: str, python: str) -> dict:
 
         r = run_command("uv", "pip", "show", package_name)
         show_metadata = Parser().parsestr(r.stdout)
-        name = show_metadata["Name"]
         version = show_metadata["Version"]
-        wheel_name = snakified(name)
+        wheel_name = snakified(show_metadata["Name"])
         full_path = Path(show_metadata["Location"]) / f"{wheel_name}-{version}.dist-info"
         metadata = get_metadata_dict(full_path / "METADATA")
         entry_points = full_path / "entry_points.txt"
@@ -97,19 +101,16 @@ def main(args=None):
     """
     Output the metadata of a package, in machine-readable format
 
-    \b
     Examples:
         uv-metadata requests
         uv-metadata .
         uv-metadata git+https://github.com/zsimic/uv-metadata@main
     """
-    # Ensure we don't let our own venv interfere with uv usage
-    os.environ.pop("VIRTUAL_ENV", None)
-    os.environ["UV_VENV_SEED"] = "0"
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--python", "-p", default=CURRENT_PY, help=f"Python interpreter to use (default: {CURRENT_PY})")
-    parser.add_argument("--key", "-k", help="Show only this key from the metadata")
-    parser.add_argument("package", default=".", nargs="?", help="Show metadata for this package")
+    parser = argparse.ArgumentParser(description=main.__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    syspy = f"{sys.version_info[0]}.{sys.version_info[1]}"
+    parser.add_argument("-p", "--python", default=syspy, help="Python interpreter to use (default: %(default)s)")
+    parser.add_argument("-k", "--key", help="Show only this key from the metadata")
+    parser.add_argument("package", default=".", nargs="?", help="Show metadata for specified package")
     args = parser.parse_args(args=args)
 
     key = args.key
@@ -120,9 +121,7 @@ def main(args=None):
 
     metadata = get_metadata(package, python)
     if key:
-        if key not in metadata:
-            abort(f"'{key}' not found in metadata")
-
+        abort_if(key not in metadata, f"'{key}' not found in metadata")
         text = metadata[key]
 
     else:
